732c24db8e7b3e4a7ce35508efe0aed73dc2c63e
shinian <shinian>
2017-11-29 16:35:27 +0800

    数组完

--- src/com/itmasir/demo/util/Generator.java
+++ src/com/itmasir/demo/util/Generator.java
@@ -5,7 +5,6 @@ package com.itmasir.demo.util;
  * @version V1.0
  * @Title: ${FILE_NAME}
  * @Package com.shinian.demo
- * @Description: TODO
  * @date 2017/8/12 17:34
  */
 public interface Generator<T> {
